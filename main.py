26055cf32c35702236102ae72affb290bf147298
Tanisha <tanisha.dua244@gmail.com>
2026-06-06 01:37:34 +0530

    Day 2: added /about and /generate with Pydantic Validation

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {"message": "Hello! My AI PM Assistant is alive!"}
+
+
+@app.get("/about")
+def about():
+    return {
+        "project": "AI PM Assistant",
+        "version": "1.0",
+        "description": "Generates PRDs, user stories and feature plans using AI"
+    }
+
+
+class ProductRequest(BaseModel):
+    product_name: str
+    product_description: str
+
+
+@app.post("/generate")
+def generate(request: ProductRequest):
+    return {
+        "product_name": request.product_name,
+        "message": f"You want to generate a PRD for: {request.product_name}",
+        "description_received": request.product_description,
+        "status": "AI generation coming in Week 2!"
+    }
